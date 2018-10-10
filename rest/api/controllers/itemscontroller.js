7f8a5fe1ca2de0c2fecf2336dd77ffa028cad7e0
Deepak Kumar <deepakkumarjha@outlook.com>
2018-10-10 18:51:09 +0530

    CURD methods added

--- rest/api/controllers/itemscontroller.js
+++ rest/api/controllers/itemscontroller.js
@@ -1,5 +1,6 @@
 const item=require('../models/itemModel');
 const mongoose=require('mongoose');
+// method to add an item
 exports.add_an_item = function (req,res,next){
    const itemData=new item({
     _id:new mongoose.Types.ObjectId(),
@@ -12,4 +13,46 @@ exports.add_an_item = function (req,res,next){
    }).catch((err)=>{
     res.send(err);
    });
+}
+
+// Method to update an element
+exports.update_an_item = function (req,res,next){
+  const id = req.params.productId;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  itemData.update({ _id: id }, { $set: updateOps })
+    .exec()
+    .then(result => {
+      res.status(200).json({
+        message: "Item updated"
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+}
+
+// Method to delete an item
+
+exports.delete_item= function(req,res,next){
+  const id = req.params.productId;
+  itemData.remove({ _id: id })
+    .exec()
+    .then(result => {
+      res.status(200).json({
+        message: "Product deleted"
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+
 }
